e8dbd85d7e4a975cb4a048cf8db0a4e418c18aa8
saihajvir <saihajvir@hotmail.com>
2021-03-23 00:23:37 -0700

    finished basic layout and content, sheeeeeesh

--- src/comps/ColorPage.js
+++ src/comps/ColorPage.js
@@ -7,7 +7,7 @@ import React, { useState } from 'react';
 const Container = styled.div`
 
     width: 100vw;
-    height: 100vh;
+    height: 82vh;
     display: flex;
     align-items: center;
     transition: 0.5s ease;
@@ -49,7 +49,7 @@ const ColorContainer = styled.div
 `
     height: 100vh;
     width: 100vw;
-    background-color: lightgrey;
+    background-color: white;
     display: flex;
     align-items: center;
     flex-direction: column;
@@ -61,7 +61,7 @@ const ColorBoxes = styled.div
     display: flex;
     width: 80%;
     height: 40%;
-    background-color: yellow;
+    background-color: white;
     justify-content: space-between;
     align-items: center;
 
@@ -71,7 +71,7 @@ const ColorText = styled.div
     display: flex;
     width: 80%;
     height: 50%;
-    background-color: blue;
+    background-color: white;
 `
 
 const CyanBox = styled.div
@@ -126,8 +126,62 @@ const GreenBox = styled.div
     border-radius: 10px;
 `
 
-export const ColorPage = () => {
+const CyanTextBox = styled.div
+`
+    display: flex;
+    flex-direction: column;
+    justify-content: left;
+`
 
+const PurpleTextBox = styled.div
+`
+    display: none;
+    flex-direction: column;
+    justify-content: left;
+`
+const BlueTextBox = styled.div
+`
+    display: none;
+    flex-direction: column;
+    justify-content: left;
+`
+const OrangeTextBox = styled.div
+`
+    display: none;
+    flex-direction: column;
+    justify-content: left;
+`
+const BlackTextBox = styled.div
+`
+    display: none;
+    flex-direction: column;
+    justify-content: left;
+`
+const GreenTextBox = styled.div
+`
+    display: none;
+    flex-direction: column;
+    justify-content: left;
+`
+const ColorName = styled.h1
+`
+`
+
+const ColorHex = styled.h3
+`
+`
+
+const ColorRgb = styled.h3
+`
+    margin-top: -10px;
+`
+
+const ColorDesc = styled.div
+`
+
+`
+
+export const ColorPage = () => {
 
 
     return (
@@ -151,7 +205,49 @@ export const ColorPage = () => {
                 </ColorBoxes>
 
                 <ColorText>
-                
+                    <CyanTextBox>
+                        <ColorName>Synthetic Cyan</ColorName>
+                        <ColorHex>#36B9C5</ColorHex>
+                        <ColorRgb>(54, 185, 197)</ColorRgb>
+                        <ColorDesc>Synthetic Cyan will be used for Synthetic Polymer fabrics.</ColorDesc>
+                    </CyanTextBox>
+
+                    <PurpleTextBox>
+                        <ColorName>Pastel Purple</ColorName>
+                        <ColorHex>#AF75BD</ColorHex>
+                        <ColorRgb>(175, 117, 189)</ColorRgb>
+                        <ColorDesc>Pastel Purple will be used for Natural Polymer fabrics.</ColorDesc>
+                    </PurpleTextBox>
+
+                    <BlueTextBox>
+                        <ColorName>Ocean Blue</ColorName>
+                        <ColorHex>#3884FF</ColorHex>
+                        <ColorRgb>(56, 132, 255)</ColorRgb>
+                        <ColorDesc>Ocean Blue will be used for all navigation buttons.</ColorDesc>
+                    </BlueTextBox>
+
+                    <OrangeTextBox>
+                        <ColorName>Scorched Orange</ColorName>
+                        <ColorHex>#C2600F</ColorHex>
+                        <ColorRgb>(194, 96, 15)</ColorRgb>
+                        <ColorDesc>Scorched Orange will be used for Animal Fiber fabrics.</ColorDesc>
+                    </OrangeTextBox>
+
+                    <BlackTextBox>
+                        <ColorName>Almost Black</ColorName>
+                        <ColorHex>#2E2E2E</ColorHex>
+                        <ColorRgb>(46, 46, 46)</ColorRgb>
+                        <ColorDesc>Almost Black will be used for descriptive text.</ColorDesc>
+                    </BlackTextBox>
+
+                    <GreenTextBox>
+                        <ColorName>Eco Green</ColorName>
+                        <ColorHex>#51B27E</ColorHex>
+                        <ColorRgb>(81, 178, 126)</ColorRgb>
+                        <ColorDesc>Eco Green is the main color in our logo, as well as the color for plant fibers.</ColorDesc>
+                    </GreenTextBox>
+
+                    
                 </ColorText>
             </ColorContainer>
 
